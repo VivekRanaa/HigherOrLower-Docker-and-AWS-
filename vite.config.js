c22fb57abef33bb40f002f6792783d9450b93c15
Vivek Rana <140080051+VivekRanaa@users.noreply.github.com>
2023-10-08 01:53:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import react from '@vitejs/plugin-react'
-import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Allow access from outside the container
+    port: 5173, // Set the port to match your Vite.js application
+  },
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the production build
-    minify: 'terser', // Minify the production build using Terser
-    sourcemap: false, // Disable source maps in production for better performance
+    outDir: 'dist',
+    minify: 'terser',
+    sourcemap: false,
   },
 })
-
