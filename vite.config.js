d61b712041e95e5f5a3e6d3b77d02d10303dcf00
Vivekranaa <vrana3325@gmail.com>
2023-10-08 01:39:01 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+    minify: 'terser', // Minify the production build using Terser
+    sourcemap: false, // Disable source maps in production for better performance
+  },
 })
+
